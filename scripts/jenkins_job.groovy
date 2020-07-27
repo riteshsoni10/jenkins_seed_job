7250972ec9cb39b332f00b3f54b56898e7d1a622
Ritesh Kumar <riteshk.296@gmail.com>
2020-07-28 04:50:26 +0530

    Update jenkins_job.groovy

--- scripts/jenkins_job.groovy
+++ scripts/jenkins_job.groovy
@@ -1,5 +1,8 @@
 pipelineJob('web_application_deployment'){
     description('Web Application Deployment using Jenkins Pipeline')
+    properties {
+        githubProjectUrl('https://github.com/riteshsoni10/demo_website.git')
+    }
     definition {
         triggers {
         scm('* * * * *')
