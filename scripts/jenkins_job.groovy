17a21c6b0707371155d3bfb65202260c5f0c8985
Ritesh Kumar <riteshk.296@gmail.com>
2020-07-30 04:00:44 +0530

    Update jenkins_job.groovy

--- scripts/jenkins_job.groovy
+++ scripts/jenkins_job.groovy
@@ -1,22 +1,67 @@
-pipelineJob('web_application_deployment'){
-    description('Web Application Deployment using Jenkins Pipeline')
+def repo_url=getBinding().getVariables()['GIT_URL']
+
+
+pipelineJob('test-env'){
+    description('Web Application Deployment in Test Environment using Jenkins Pipeline')
     properties {
-        githubProjectUrl('https://github.com/riteshsoni10/demo_website.git')
+        githubProjectUrl("$repo_url")
     }
     definition {
         triggers {
-             githubPush()
+             scm(* * * * *)
         }
        cpsScm {
             scm {
                 git{
                     remote {
-                        url('https://github.com/riteshsoni10/demo_website.git')
+                        url("$repo_url")
                     }
-                    branch('*/master')     
+                    branch('*/develop') 
+                    extensions {
+                        userIdentity {
+                            // If given, "git config user.name [this]" is called before builds.
+                            name("deployment_server")
+                            // If given, "git config user.email [this]" is called before builds.
+                            email("jenkins@organisation.com")
+                        } 
+                    }    
                 }
             }
             lightweight()
         }
     }
 }
+
+pipelineJob('prod-env'){
+    description('Web Application Deployment in Production Environment using Jenkins Pipeline')
+    properties {
+        githubProjectUrl("$repo_url")
+    }
+    definition {
+        triggers {
+            //githubPush()
+            scm(* * * * *)
+        }
+       cpsScm {
+            scm {
+                git{
+                    remote {
+                        url("$repo_url")
+                    }
+                    branch('*/master')
+                    extensions{
+                        userIdentity {
+                            // If given, "git config user.name [this]" is called before builds.
+                            name("deployment_server")
+                            // If given, "git config user.email [this]" is called before builds.
+                            email("jenkins@organisation.com")
+                        } 
+                    }     
+                }
+            }
+            lightweight()
+        }
+    }
+}
+
+
